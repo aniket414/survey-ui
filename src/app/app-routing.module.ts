aafb0ccf2ea2dbec9f62b86262f7103868becc40
Aniket Pandey <aniket414@gmail.com>
2022-05-05 05:01:10 -0400

    added UI project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubmitSurveyComponent } from './submit-survey/submit-survey.component';
+import { SurveyListComponent } from './survey-list/survey-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'surveys', component: SurveyListComponent},
+  {path: 'submit-survey', component: SubmitSurveyComponent},
+  // {path: '', redirectTo: 'surveys', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SurveyListComponent } from './survey-list/survey-list.component';
+import { SubmitSurveyComponent } from './submit-survey/submit-survey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyListComponent,
+    SubmitSurveyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
